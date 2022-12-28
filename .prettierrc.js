4e7dff91b5fb02e0f2f5fea8f2deb60251a13a35
Paul Pop <paul.pop@luminos.software>
2022-12-28 16:01:10 +0200

    Tailwind installed

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    printWidth: 120
+  };
+  
