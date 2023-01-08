038a0b9bad5c435d8f173b5b6a75a928b5aae09d
Paul Pop <paul.pop@luminos.software>
2023-01-08 20:24:13 +0200

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Hero } from './components';
+import { heroapi } from './data/data';
 
 const App = () => {
-  return <Hero />;
+  return <Hero heroapi={heroapi} />;
 };
 
 export default App;
