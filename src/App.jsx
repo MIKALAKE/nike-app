1a7e39f9e1d9bd62fb3b727b0d4734919e88067f
Pop Paul <paul.pop@luminos.software>
2023-01-18 20:52:11 +0200

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import { FlexContent, Footer, Hero, Sales, Stories } from './components';
+import { FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
 import { footerAPI, heroapi, highlight, popularsales, sneaker, story, toprateslaes } from './data/data';
 
 const App = () => {
   return (
-    <main className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi} />
-      <Sales sales={popularsales} ifExists />
-      <FlexContent endpoint={highlight} ifExists />
-      <Sales sales={toprateslaes} />
-      <FlexContent endpoint={sneaker} />
-      <Stories story={story} />
-      <Footer footerAPI={footerAPI} />
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales sales={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales sales={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+        <Footer footerAPI={footerAPI} />
+      </main>
+    </>
   );
 };
 
